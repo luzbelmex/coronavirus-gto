8e56f757f637256241d4eb9cf61ef3dc42cc6dca
Samuel Arrieta <francisco.samuel.a.t@gmail.com>
2020-03-31 22:48:15 -0600

    se separa codigo en otro archivo para las ciudades

---
+++ js/ciudades.js
@@ -0,0 +1,111 @@
+var acambaro = L.circle([20.035770, -100.716935],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 0
+}).addTo(map).bindPopup('Acámbaro');
+
+var celaya = L.circle([20.525643, -100.813928],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 400
+}).addTo(map).bindPopup('Celaya');
+
+var cortazar = L.circle([20.478907, -100.959399],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 0
+}).addTo(map).bindPopup('Cortazar');
+
+var dolores = L.circle([21.156256, -100.934015],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 0
+}).addTo(map).bindPopup('Dolores Hidalgo');
+
+var guanajuato = L.circle([21.018412, -101.255928],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 200
+}).addTo(map).bindPopup('Guanajuato');
+
+var irapuato = L.circle([20.675245, -101.344379],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 600
+}).addTo(map).bindPopup('Irapuato');
+
+var leon = L.circle([21.121830, -101.682481],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 2800
+}).addTo(map).bindPopup('León');
+
+var ocampo = L.circle([21.647678, -101.478420],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 0
+}).addTo(map).bindPopup('Ocampo');
+
+var penjamo = L.circle([20.431298, -101.722123],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 0
+}).addTo(map).bindPopup('Pénjamo');
+
+var salamanca = L.circle([20.572517, -101.191197],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 200
+}).addTo(map).bindPopup('Salamanca');
+
+var salvatierra = L.circle([20.211976, -100.880958],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 0
+}).addTo(map).bindPopup('Salvatierra');
+
+var sfr = L.circle([21.018723, -101.857413],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 0
+}).addTo(map).bindPopup('San Fancisco del Rincón');
+
+var slp = L.circle([21.297578, -100.517204],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 100
+}).addTo(map).bindPopup('San Luis de la Paz');
+
+var sma = L.circle([20.913036, -100.745744],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 100
+}).addTo(map).bindPopup('San Miguel de Ayende');
+
+var silao = L.circle([20.947407, -101.425863],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 100
+}).addTo(map).bindPopup('Silao');
+
+var vds = L.circle([20.390977, -101.193464],{
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 100
+}).addTo(map).bindPopup('Valle de Santiago');

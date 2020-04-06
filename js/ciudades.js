e474f99fe40a78f47f5f264fc4c1df9ab5cd0dba
Samuel Arrieta <francisco.samuel.a.t@gmail.com>
2020-04-06 16:19:39 -0500

    se actualizan datos

--- js/ciudades.js
+++ js/ciudades.js
@@ -1,34 +1,34 @@
 var abasolo = L.circle([20.452199, -101.529024],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: 'red',
+    fillColor: '#f03',
     fillOpacity: 0.5,
-    radius: 200
+    radius: 100
 }).addTo(map).bindPopup('Abasolo');
 
 var acambaro = L.circle([20.035770, -100.716935],{
     color: '#ffc300',
     fillColor: '#ffcd2b',
     fillOpacity: 0.5,
-    radius: 600
+    radius: 300
 }).addTo(map).bindPopup('Acámbaro');
 
 var apaseoA = L.circle([20.458464, -100.621415],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
     radius: 100
 }).addTo(map).bindPopup('Apaseo el Alto');
 
 var apaseoG = L.circle([20.545219, -100.686487],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
     radius: 100
 }).addTo(map).bindPopup('Apaseo el Grande');
 
 var atargea = L.circle([21.266927, -99.719153],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
     radius: 100
 }).addTo(map).bindPopup('Atargea');
@@ -62,10 +62,10 @@ var cortazar = L.circle([20.478907, -100.959399],{
 }).addTo(map).bindPopup('Cortazar');
 
  var cueramaro = L.circle([20.626446, -101.671127],{
-     color: '#ffc300',
-     fillColor: '#ffcd2b',
+     color: '#1e8449',
+     fillColor: '#1e8449',
     fillOpacity: 0.5,
-    radius: 400
+    radius: 0
 }).addTo(map).bindPopup('Cueramaro');
 
  var doctor = L.circle([21.142381, -100.319464],{
@@ -90,8 +90,8 @@ var guanajuato = L.circle([21.018412, -101.255928],{
 }).addTo(map).bindPopup('Guanajuato');
 
  var huanimaro = L.circle([20.368187, -101.496742],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
     radius: 100
 }).addTo(map).bindPopup('Huanimaro');
@@ -104,17 +104,17 @@ var irapuato = L.circle([20.675245, -101.344379],{
 }).addTo(map).bindPopup('Irapuato');
 
  var jaral = L.circle([20.371957, -101.064334],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 200
+    radius: 0
 }).addTo(map).bindPopup('Jaral del Progreso');
 
  var jerecuaro = L.circle([20.150831, -100.508798],{
-     color: '#ffc300',
-     fillColor: '#ffcd2b',
+     color: '#1e8449',
+     fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Jerecuaro');
 
 var leon = L.circle([21.121830, -101.682481],{
@@ -125,87 +125,87 @@ var leon = L.circle([21.121830, -101.682481],{
 }).addTo(map).bindPopup('León');
 
  var manuelDoblado = L.circle([20.729548, -101.953144],{
-     color: '#ffc300',
-     fillColor: '#ffcd2b',
+     color: '#1e8449',
+     fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Manuel Doblado');
 
  var moroleon = L.circle([20.125079, -101.196217],{
-     color: '#ffc300',
-     fillColor: '#ffcd2b',
+     color: '#1e8449',
+     fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Moroleon');
 
 var ocampo = L.circle([21.647678, -101.478420],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Ocampo');
 
 var penjamo = L.circle([20.431298, -101.722123],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 200
+    radius: 0
 }).addTo(map).bindPopup('Pénjamo');
 
  var pueblo = L.circle([20.526844, -101.371324],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Pueblo Nuevo');
 
  var purisima = L.circle([20.889676, -101.912435],{
-     color: '#ffc300',
-     fillColor: '#ffcd2b',
+     color: '#1e8449',
+     fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Purisima del Rincon');
 
  var romita = L.circle([20.872123, -101.51667],{
-     color: '#ffc300',
-     fillColor: '#ffcd2b',
+     color: '#1e8449',
+     fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Romita');
 
 var salamanca = L.circle([20.572517, -101.191197],{
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
-    radius: 200
+    radius: 400
 }).addTo(map).bindPopup('Salamanca');
 
 var salvatierra = L.circle([20.211976, -100.880958],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Salvatierra');
 
  var sanDiego = L.circle([21.467901, -100.872875],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
     radius: 100
 }).addTo(map).bindPopup('San Diego de la Unión');
 
  var sanFelipe = L.circle([21.478435, -101.213493],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 200
+    radius: 0
 }).addTo(map).bindPopup('San Felipe');
 
 var sfr = L.circle([21.018723, -101.857413],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('San Fancisco del Rincón');
 
  var sanJose = L.circle([20.998587, -100.385738],{
@@ -237,10 +237,10 @@ var sma = L.circle([20.913036, -100.745744],{
 }).addTo(map).bindPopup('Santa Catarina');
 
  var santaCJR = L.circle([20.644057, -100.993401],{
-    color: '##ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Santa Cruz de Juventino Rosas');
 
  var santiago = L.circle([20.172562, -100.993369],{
@@ -300,22 +300,22 @@ var vds = L.circle([20.390977, -101.193464],{
 }).addTo(map).bindPopup('Victoria');
 
  var villagran = L.circle([20.516307, -100.997578],{
-     color: '#ffc300',
-     fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Villagrán');
 
  var xichu = L.circle([21.299218, -100.056449],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 0
 }).addTo(map).bindPopup('Xichú');
 
  var yuriria = L.circle([20.211997, -101.131539],{
-     color: '#ffc300',
-     fillColor: '#ffcd2b',
+    color: '#1e8449',
+    fillColor: '#239b56',
     fillOpacity: 0.5,
-    radius: 200
+    radius: 0
 }).addTo(map).bindPopup('Yuriria');
--- js/graficas.js
+++ js/graficas.js
@@ -3,21 +3,28 @@ var contagiados = document.getElementById('contagiados').getContext('2d');
 var lineaContagiados = new Chart( contagiados, {
     type: 'line',
     data: {
-      labels: ['15-Marzo','16-Marzo', '17-Marzo', '18-Marzo', '19-Marzo', '20-Marzo', '21-Marzo', '22-Marzo', '23-Marzo', '24-Marzo', '25-Marzo', '26-Marzo', '27-Marzo', '28-Marzo', '29-Marzo', '30-Marzo', '31-Marzo', '1-Abril', '2-Abril', '3-Abril', '4-Abril'],
+      labels: ['15-Marzo','16-Marzo', '17-Marzo', '18-Marzo', '19-Marzo', '20-Marzo', '21-Marzo', '22-Marzo', '23-Marzo', '24-Marzo', '25-Marzo', '26-Marzo', '27-Marzo', '28-Marzo', '29-Marzo', '30-Marzo', '31-Marzo', '1-Abril', '2-Abril', '3-Abril', '4-Abril', '5-Abril', '6-Abril'],
       datasets: [{
         label: 'Contagiados',
-        data: [0, 1, 1, 4, 4, 4, 4, 7, 9, 13, 25, 31, 31, 37, 37, 43, 46, 49, 50, 52, 54 ],
+        data: [0, 1, 1, 4, 4, 4, 4, 7, 9, 13, 25, 31, 31, 37, 37, 43, 46, 49, 50, 52, 54, 54, 58 ],
         backgroundColor:'rgba(255, 0, 0, 0.0)',
         borderColor: 'rgb(255, 0, 0)',
         borderWidth: 3
       }, // confirmados
       {
         label: 'Recuperados',
-        data: [, , , , , , , , , , , , , , , , 0, 16, 16, 16, 17 ],
+        data: [, , , , , , , , , , , , , , , , 0, 16, 16, 16, 17, 17, 17 ],
         backgroundColor:'rgba(35, 155, 86, 0.0)',
         borderColor: 'rgb(35, 155, 86)',
         borderWidth: 3
-      } // recuperados
+      }, // recuperados
+      {
+        label: 'Fallecidos',
+        data: [, , , , , , , , , , , , , , , , , , , , , 0, 1 ],
+        backgroundColor: 'rgba(77, 77, 77, 0.0)',
+        borderColor: 'rgb(77, 77, 77)',
+        borderWidth: 3
+      }
     ]
     }, // data
 });
@@ -27,10 +34,10 @@ var sospechosos = document.getElementById('sospechosos').getContext('2d');
 var lineaSospechosos = new Chart(sospechosos, {
   type: 'line',
   data: {
-    labels: ['1-feb', '2-feb', '3-feb', '4-feb', '5-feb', '6-feb', '7-feb', '8-feb', '9-feb', '10-feb', '11-feb', '12-feb', '13-feb', '14-feb', '15-feb', '16-feb', '17-feb', '18-feb', '19-feb', '20-feb', '21-feb', '22-feb', '23-feb', '24-feb', '25-feb', '26-feb', '27-feb', '28-feb', '29-feb', '1-Marzo', '2-Marzo', '3-Marzo', '4-Marzo', '5-Marzo', '6-Marzo', '7-Marzo', '8-Marzo', '9-Marzo', '10-Marzo', '11-Marzo', '12-Marzo', '13-Marzo', '14-Marzo', '15-Marzo', '16-Marzo', '17-Marzo', '18-Marzo', '19-Marzo', '20-Marzo', '21-Marzo', '22-Marzo', '23-Marzo', '24-Marzo', '25-Marzo', '26-Marzo', '27-Marzo', '28-Marzo', '29-Marzo', '30-Marzo', '31-Marzo', '1-Abril', '2-Abril', '3-Abril', '4-Abril'],
+    labels: ['1-feb', '2-feb', '3-feb', '4-feb', '5-feb', '6-feb', '7-feb', '8-feb', '9-feb', '10-feb', '11-feb', '12-feb', '13-feb', '14-feb', '15-feb', '16-feb', '17-feb', '18-feb', '19-feb', '20-feb', '21-feb', '22-feb', '23-feb', '24-feb', '25-feb', '26-feb', '27-feb', '28-feb', '29-feb', '1-Marzo', '2-Marzo', '3-Marzo', '4-Marzo', '5-Marzo', '6-Marzo', '7-Marzo', '8-Marzo', '9-Marzo', '10-Marzo', '11-Marzo', '12-Marzo', '13-Marzo', '14-Marzo', '15-Marzo', '16-Marzo', '17-Marzo', '18-Marzo', '19-Marzo', '20-Marzo', '21-Marzo', '22-Marzo', '23-Marzo', '24-Marzo', '25-Marzo', '26-Marzo', '27-Marzo', '28-Marzo', '29-Marzo', '30-Marzo', '31-Marzo', '1-Abril', '2-Abril', '3-Abril', '4-Abril', '5-Abril', '6-Abril'],
     datasets: [{
       label: 'Casos Sospechosos',
-      data: [2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 5, 5, 5, 6, 8, 8, 8, 10, 11, 11, 14, 15, 16, 17, 18, 21, 23, 24, 28, 35, 48, 61, 67, 101, 117, 139, 164, 198, 234, 277, 308, 369, 398, 460, 513, 564, 622, 709, 788, 859, 945, 1015, 1069, 1088, 1100, 1104, 1104],
+      data: [2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 5, 5, 5, 6, 8, 8, 8, 10, 11, 11, 14, 15, 16, 17, 18, 21, 23, 24, 28, 35, 48, 61, 67, 101, 117, 139, 164, 198, 234, 277, 308, 369, 398, 460, 513, 564, 622, 709, 788, 860, 947, 1031, 1091, 1114, 1129, 1133, 1134, 1134],
       backgroundColor: 'rgba(255, 195, 0, 0.3)',
       borderColor: '#ffc300',
       borderWidth: 3
@@ -43,26 +50,32 @@ var ciudades = document.getElementById('ciudades').getContext('2d');
 var lineaCiudades = new Chart( ciudades, {
   type: 'bar',
   data: {
-    labels: ['Celaya', 'Cortazar', 'Guanajuato', 'Irapuato', 'León', 'Salamanca', 'San Luis de la Paz', 'San Miguel de Ayende', 'Silao', 'Valle de Santiago'],
+    labels: ['Abasolo', 'Celaya', 'Cortazar', 'Guanajuato', 'Irapuato', 'León', 'Salamanca', 'San Luis de la Paz', 'San Miguel de Ayende', 'Silao', 'Valle de Santiago'],
     datasets: [{
       label: 'Contagiados',
-      data: [4, 1, 4, 8, 30, 2, 1, 2, 1, 1],
+      data: [1, 4, 1, 4, 8, 30, 5, 1, 2, 1, 1],
       backgroundColor: 'rgba(255, 0, 0, .7)',
       borderColor: 'rgb(255, 0, 0)',
       borderWidth: 1
     },
     {
       label: 'Recuperados',
-      data: [0, 0,  0, 1, 16, 0, 0, 0, 0, 0],
+      data: [0, 0, 0,  0, 1, 16, 0, 0, 0, 0, 0],
       backgroundColor: 'rgba(35, 155, 86, .7)',
       borderColor: 'rgb(35, 155, 86)',
       borderWidth: 1
     },
     {
       label: 'Sospechosos',
-      data: [8, 4, 1, 14, 138, 7, 0, 2, 1, 0],
+      data: [2, 6, 2, 1, 10, 41, 6, 0, 2, 0, 0],
       backgroundColor: 'rgba(255, 205, 43, 0.5)',
       borderColor: 'rgb(255, 205, 43)',
+    },
+    {
+      label: 'Fallecidos',
+      data: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+      backgroundColor: 'rgba(77, 77, 77, 0.5)',
+      borderColor: 'rgb(77, 77, 77)',
     }
   ],
 

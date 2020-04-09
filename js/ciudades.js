35c7412bd3f3bcd21ecc00ccdb25ccd676210760
Samuel Arrieta <francisco.samuel.a.t@gmail.com>
2020-04-09 18:18:40 -0500

    se actualizan datos

--- js/ciudades.js
+++ js/ciudades.js
@@ -2,28 +2,28 @@ var abasolo = L.circle([20.452199, -101.529024],{
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 200
 }).addTo(map).bindPopup('Abasolo');
 
 var acambaro = L.circle([20.035770, -100.716935],{
     color: '#ffc300',
     fillColor: '#ffcd2b',
     fillOpacity: 0.5,
-    radius: 800
+    radius: 500
 }).addTo(map).bindPopup('Acámbaro');
 
 var apaseoA = L.circle([20.458464, -100.621415],{
     color: '#ffc300',
     fillColor: '#ffcd2b',
     fillOpacity: 0.5,
-    radius: 100
+    radius: 200
 }).addTo(map).bindPopup('Apaseo el Alto');
 
 var apaseoG = L.circle([20.545219, -100.686487],{
     color: '#ffc300',
     fillColor: '#ffcd2b',
     fillOpacity: 0.5,
-    radius: 300
+    radius: 400
 }).addTo(map).bindPopup('Apaseo el Grande');
 
 var atargea = L.circle([21.266927, -99.719153],{
@@ -37,7 +37,7 @@ var celaya = L.circle([20.525643, -100.813928],{
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
-    radius: 500
+    radius: 400
 }).addTo(map).bindPopup('Celaya');
 
 var comonfort = L.circle([20.714684, -100.758431],{
@@ -62,10 +62,10 @@ var cortazar = L.circle([20.478907, -100.959399],{
 }).addTo(map).bindPopup('Cortazar');
 
  var cueramaro = L.circle([20.626446, -101.671127],{
-     color: '#1e8449',
-     fillColor: '#1e8449',
-    fillOpacity: 0.5,
-    radius: 0
+     color: '#ffc300',
+     fillColor: '#ffcd2b',
+     fillOpacity: 0.5,
+     radius: 100
 }).addTo(map).bindPopup('Cueramaro');
 
  var doctor = L.circle([21.142381, -100.319464],{
@@ -79,21 +79,21 @@ var dolores = L.circle([21.156256, -100.934015],{
     color: '#ffc300',
     fillColor: '#ffcd2b',
     fillOpacity: 0.5,
-    radius: 200
+    radius: 400
 }).addTo(map).bindPopup('Dolores Hidalgo');
 
 var guanajuato = L.circle([21.018412, -101.255928],{
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
-    radius: 500
+    radius: 400
 }).addTo(map).bindPopup('Guanajuato');
 
  var huanimaro = L.circle([20.368187, -101.496742],{
-     color: '#ffc300',
-     fillColor: '#ffcd2b',
+     color: '#1e8449',
+     fillColor: '#239b56',
      fillOpacity: 0.5,
-     radius: 100
+     radius: 0
 }).addTo(map).bindPopup('Huanimaro');
 
 var irapuato = L.circle([20.675245, -101.344379],{
@@ -114,42 +114,42 @@ var irapuato = L.circle([20.675245, -101.344379],{
      color: '#ffc300',
      fillColor: '#ffcd2b',
      fillOpacity: 0.5,
-     radius: 300
+     radius: 500
 }).addTo(map).bindPopup('Jerecuaro');
 
 var leon = L.circle([21.121830, -101.682481],{
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
-    radius: 1300
+    radius: 1200
 }).addTo(map).bindPopup('León');
 
  var manuelDoblado = L.circle([20.729548, -101.953144],{
      color: '#ffc300',
      fillColor: '#ffcd2b',
      fillOpacity: 0.5,
-     radius: 300
+     radius: 200
 }).addTo(map).bindPopup('Manuel Doblado');
 
  var moroleon = L.circle([20.125079, -101.196217],{
      color: '#ffc300',
      fillColor: '#ffcd2b',
      fillOpacity: 0.5,
-     radius: 200
+     radius: 300
 }).addTo(map).bindPopup('Moroleon');
 
 var ocampo = L.circle([21.647678, -101.478420],{
     color: '#ffc300',
     fillColor: '#ffcd2b',
     fillOpacity: 0.5,
-    radius: 200
+    radius: 300
 }).addTo(map).bindPopup('Ocampo');
 
 var penjamo = L.circle([20.431298, -101.722123],{
-    color: '#1e8449',
-    fillColor: '#239b56',
+    color: '#ffc300',
+    fillColor: '#ffcd2b',
     fillOpacity: 0.5,
-    radius: 0
+    radius: 200
 }).addTo(map).bindPopup('Pénjamo');
 
  var pueblo = L.circle([20.526844, -101.371324],{
@@ -163,35 +163,35 @@ var penjamo = L.circle([20.431298, -101.722123],{
      color: '#ffc300',
      fillColor: '#ffcd2b',
      fillOpacity: 0.5,
-     radius: 100
+     radius: 200
 }).addTo(map).bindPopup('Purisima del Rincon');
 
  var romita = L.circle([20.872123, -101.51667],{
      color: '#ffc300',
      fillColor: '#ffcd2b',
      fillOpacity: 0.5,
-     radius: 300
+     radius: 200
 }).addTo(map).bindPopup('Romita');
 
 var salamanca = L.circle([20.572517, -101.191197],{
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
-    radius: 1500
+    radius: 1600
 }).addTo(map).bindPopup('Salamanca');
 
 var salvatierra = L.circle([20.211976, -100.880958],{
     color: '#ffc300',
     fillColor: '#ffcd2b',
     fillOpacity: 0.5,
-    radius: 300
+    radius: 200
 }).addTo(map).bindPopup('Salvatierra');
 
  var sanDiego = L.circle([21.467901, -100.872875],{
      color: '#ffc300',
      fillColor: '#ffcd2b',
      fillOpacity: 0.5,
-     radius: 200
+     radius: 100
 }).addTo(map).bindPopup('San Diego de la Unión');
 
  var sanFelipe = L.circle([21.478435, -101.213493],{
@@ -209,15 +209,15 @@ var sfr = L.circle([21.018723, -101.857413],{
 }).addTo(map).bindPopup('San Fancisco del Rincón');
 
  var sanJose = L.circle([20.998587, -100.385738],{
-    color: '#1e8449',
-    fillColor: '#239b56',
-    fillOpacity: 0.5,
-    radius: 0
+     color: '#ffc300',
+     fillColor: '#ffcd2b',
+     fillOpacity: 0.5,
+     radius: 100
 }).addTo(map).bindPopup('San Jose Iturbide');
 
 var slp = L.circle([21.297578, -100.517204],{
-    color: 'red',
-    fillColor: '#f03',
+    color: '#ffc300',
+    fillColor: '#ffcd2b',
     fillOpacity: 0.5,
     radius: 100
 }).addTo(map).bindPopup('San Luis de la Paz');
@@ -279,10 +279,10 @@ var silao = L.circle([20.947407, -101.425863],{
 }).addTo(map).bindPopup('Tierra Blanca');
 
  var uriangato = L.circle([20.141507, -101.173776],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
-    fillOpacity: 0.5,
-    radius: 100
+     color: '#1e8449',
+     fillColor: '#239b56',
+     fillOpacity: 0.5,
+     radius: 0
 }).addTo(map).bindPopup('Uriangato');
 
 var vds = L.circle([20.390977, -101.193464],{
@@ -314,8 +314,8 @@ var vds = L.circle([20.390977, -101.193464],{
 }).addTo(map).bindPopup('Xichú');
 
  var yuriria = L.circle([20.211997, -101.131539],{
-    color: '#ffc300',
-    fillColor: '#ffcd2b',
+    color: '#red',
+    fillColor: '#f03',
     fillOpacity: 0.5,
     radius: 100
 }).addTo(map).bindPopup('Yuriria');
